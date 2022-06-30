ef3ed070e29649dd8fcdf74616f65788730c07f3
Lauren <laurenastreff@gmail.com>
2022-06-30 00:05:47 -0500

    buttons on quiz page.

---
+++ e.l.app/QuizViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuizViewController.swift
+//  e.l.app
+//
+//  Created by Lauren Streff on 6/29/22.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
