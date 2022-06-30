bf5a0ad54bdd1c421948c8154a164c8cd3d835a8
Lauren <laurenastreff@gmail.com>
2022-06-29 21:47:18 -0500

    new VCs

---
+++ e.l.app/Scroll.swift
@@ -0,0 +1,23 @@
+//
+//  Scroll.swift
+//  e.l.app
+//
+//  Created by Lauren Streff on 6/29/22.
+//
+
+import UIKit
+
+class Scroll: UIScrollView {
+   
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing
+     //code@interface UIScrollView : UIView
+    }
+    */
+
+}
